3209afed248984e46f4bf576b034e08a210e6a9d
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2024-03-03 14:33:37 -0500

    Working service for dropper

--- include/hardware_interface_manager/RS485Interface.h
+++ include/hardware_interface_manager/RS485Interface.h
@@ -7,6 +7,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sonia_common_cpp/SerialConn.h"
 #include "sonia_common_ros2/msg/serial_message.hpp"
+#include "sonia_common_ros2/srv/dropper_service.hpp"
 #include "SharedQueue.h"
 
 namespace sonia_hw_interface {
@@ -28,6 +29,33 @@ namespace sonia_hw_interface {
 
 
         private:
+            enum _SlaveId: uint8_t {
+                SLAVE_PSU0 = 0,
+                SLAVE_PSU1 = 1,
+                SLAVE_PSU2 = 2,
+                SLAVE_PSU3 = 3,
+                SLAVE_KILLMISSION = 4,
+                SLAVE_ESC = 5,
+                SLAVE_IO = 6,
+                SLAVE_STATE_SCREEN = 7,
+                SLAVE_PWR_MANAGEMENT = 8,
+            };
+            enum _Cmd: uint8_t {
+                CMD_MISSION = 0,
+                CMD_KILL = 1,
+                CMD_VOLTAGE = 0,
+                CMD_CURRENT = 1,
+                CMD_TEMPERATURE = 2,
+                CMD_READ_MOTOR = 15,
+                CMD_ACT_MOTOR = 16,
+                CMD_PWM = 17,
+                CMD_IO_TEMP= 0,
+                CMD_IO_DROPPER_ACTION = 1,
+                CMD_IO_TORPEDO_ACTION = 2,
+                CMD_IO_ARM_ACTION = 3,
+                CMD_IO_LEAK_SENSOR = 4,
+                CMD_KEEP_ALIVE = 30,
+            };
             uint16_t checkSum(uint8_t slave, uint8_t cmd, uint8_t nbByte, std::vector<uint8_t> data);
 
             //void receivedData(const sonia_common::SendRS485Msg::ConstPtr &recievedData);
@@ -35,24 +63,31 @@ namespace sonia_hw_interface {
             void writeData();
             void parseData();
             // TODO add type
+            
+            /**
+            
+            */
             void processTx(const sonia_common_ros2::msg::SerialMessage &data) const;
-
+            void processDropperRequest(const std::shared_ptr<sonia_common_ros2::srv::DropperService::Request> request, std::shared_ptr<sonia_common_ros2::srv::DropperService::Response> response);
+            
             // double sleepTime;
-            sonia_cpp::SerialConn _serial_connection;
+            sonia_common_cpp::SerialConn _serial_connection;
 
             rclcpp::Subscription<sonia_common_ros2::msg::SerialMessage>::SharedPtr _subscriber;
+            rclcpp::Service<sonia_common_ros2::srv::DropperService>::SharedPtr _dropper_server;
 
             std::thread _reader;
             std::thread _parser;
             std::thread _writer;
 
-            SharedQueue<queueObject> writerQueue;
-            SharedQueue<uint8_t> parseQueue;
+            SharedQueue<queueObject> _writerQueue;
+            SharedQueue<uint8_t> _parseQueue;
 
             bool _thread_control;
 
             static const int DATA_READ_CHUNCK = 8192;
-            const u_int8_t START_BIT = 0x3A;
+            const u_int8_t _START_BYTE = 0x3A;
+            const u_int8_t _END_BYTE = 0x0D;
     };
 
 }
--- src/RS485Interface.cpp
+++ src/RS485Interface.cpp
@@ -1,22 +1,24 @@
 #include "hardware_interface_manager/RS485Interface.h"
 using std::placeholders::_1;
+using std::placeholders::_2;
 
-namespace sonia_hw_interface {
+namespace sonia_hw_interface
+{
 
     RS485Interface::RS485Interface()
-    : Node("rs485_interface"), _serial_connection("/dev/RS485", B115200, false), _thread_control(true) 
+        : Node("rs485_interface"), _serial_connection("/dev/RS485", B115200, false), _thread_control(true)
     {
         _reader = std::thread(std::bind(&RS485Interface::readData, this));
         _writer = std::thread(std::bind(&RS485Interface::writeData, this));
         _parser = std::thread(std::bind(&RS485Interface::parseData, this));
 
         _subscriber = this->create_subscription<sonia_common_ros2::msg::SerialMessage>("/rs485_interface/tx", 10, std::bind(&RS485Interface::processTx, this, _1));
+        _dropper_server = this->create_service<sonia_common_ros2::srv::DropperService>("actuate_dropper", std::bind(&RS485Interface::processDropperRequest, this, _1, _2));
     }
-    
-    //node destructor
+
+    // node destructor
     RS485Interface::~RS485Interface()
     {
-
     }
 
     bool RS485Interface::OpenPort()
@@ -26,10 +28,10 @@ namespace sonia_hw_interface {
 
     uint16_t RS485Interface::checkSum(uint8_t slave, uint8_t cmd, uint8_t nbByte, std::vector<uint8_t> data)
     {
-        uint16_t check = (uint16_t)(0x3A+slave+cmd+nbByte+0x0D);
-        for(uint8_t i=0; i<nbByte; i++)
+        uint16_t check = (uint16_t)(0x3A + slave + cmd + nbByte + 0x0D);
+        for (uint8_t i = 0; i < nbByte; i++)
         {
-            check+= (uint8_t)data[i];
+            check += (uint8_t)data[i];
         }
         return check;
     }
@@ -44,19 +46,30 @@ namespace sonia_hw_interface {
         std::cout << (int)data.id << std::endl;
     }
 
-// no need.
+    void RS485Interface::processDropperRequest(const std::shared_ptr<sonia_common_ros2::srv::DropperService::Request> request, std::shared_ptr<sonia_common_ros2::srv::DropperService::Response> response)
+    {
+        ssize_t transmit_status;
+        uint16_t checksum = checkSum(_SlaveId::SLAVE_IO, _Cmd::CMD_IO_DROPPER_ACTION, 1, std::vector<uint8_t>{request->side});
+        uint8_t checksum_1 = checksum >> 8;
+        uint8_t checksum_2 = checksum & 0xFF;
+        const uint8_t dropper[8] = {_START_BYTE, _SlaveId::SLAVE_IO, _Cmd::CMD_IO_DROPPER_ACTION, 1, request->side, checksum_1, checksum_2, _END_BYTE};
+        transmit_status = _serial_connection.Transmit(dropper, 8);
+        response->result = transmit_status;
+    }
+
+    // no need.
     void RS485Interface::readData()
     {
         uint8_t data[DATA_READ_CHUNCK];
-        while(_thread_control)
+        while (_thread_control)
         {
-           ssize_t str_len = _serial_connection.ReadPackets(DATA_READ_CHUNCK, data);
+            ssize_t str_len = _serial_connection.ReadPackets(DATA_READ_CHUNCK, data);
 
-            if(str_len != -1)
+            if (str_len != -1)
             {
-                for(ssize_t i = 0; i < str_len; i++)
+                for (ssize_t i = 0; i < str_len; i++)
                 {
-                    parseQueue.push_back((uint8_t)data[i]);
+                    _parseQueue.push_back((uint8_t)data[i]);
                 }
             }
         }
@@ -64,32 +77,33 @@ namespace sonia_hw_interface {
 
     void RS485Interface::writeData()
     {
-    
-        while(_thread_control)
+        // close the thread.
+        while (_thread_control)
         {
-            while(!writerQueue.empty())
+            // pause the thread.
+            while (!_writerQueue.empty())
             {
-                queueObject msg = writerQueue.get_n_pop_front(); //TODO Replace sonia_common
+                queueObject msg = _writerQueue.get_n_pop_front(); // TODO Replace sonia_common
 
                 const size_t data_size = msg.data.size() + 7;
-                uint8_t *data = new uint8_t[data_size] ;
-                data[0] = START_BIT;
+                uint8_t *data = new uint8_t[data_size];
+                data[0] = _START_BYTE;
                 data[1] = msg.slave;
                 data[2] = msg.cmd;
                 data[3] = (uint8_t)msg.data.size();
 
-                for(int i = 0; i < data[3]; i++)
+                for (int i = 0; i < data[3]; i++)
                 {
-                    data[i+4] = msg.data[i];
+                    data[i + 4] = msg.data[i];
                 }
 
-                uint16_t checksum = checkSum(data[1], data[2], data[3], (std::vector<uint8_t>) data[4]);
+                uint16_t checksum = checkSum(data[1], data[2], data[3], (std::vector<uint8_t>)data[4]);
 
-                data[data_size-3] = (uint8_t)(checksum >> 8);
-                data[data_size-2] = (uint8_t)(checksum & 0xFF);
-                data[data_size-1] = (uint8_t)(0x0D);
+                data[data_size - 3] = (uint8_t)(checksum >> 8);
+                data[data_size - 2] = (uint8_t)(checksum & 0xFF);
+                data[data_size - 1] = (uint8_t)(0x0D);
 
-                _serial_connection.Transmit((const uint8_t*)data, data_size);
+                _serial_connection.Transmit((const uint8_t *)data, data_size);
                 delete data;
             }
         }
@@ -97,46 +111,46 @@ namespace sonia_hw_interface {
 
     void RS485Interface::parseData()
     {
-        
-        while(_thread_control)
+
+        while (_thread_control)
         {
-            
-            //read until the start there or the queue is empty
-            while(!parseQueue.empty())
+
+            // read until the start there or the queue is empty
+            while (!_parseQueue.empty())
             {
                 // check if the bit is the start bit:
-                if(parseQueue.front() != START_BIT) 
+                if (_parseQueue.front() != _START_BYTE)
                 {
-                    parseQueue.pop_front();
+                    _parseQueue.pop_front();
                 }
                 else
                 {
                     queueObject msg;
 
-                    //pop the unused start data
-                    parseQueue.pop_front();
+                    // pop the unused start data
+                    _parseQueue.pop_front();
 
-                    msg.slave = parseQueue.get_n_pop_front();
-                    msg.cmd = parseQueue.get_n_pop_front();
-                    uint8_t nbByte = parseQueue.get_n_pop_front();
+                    msg.slave = _parseQueue.get_n_pop_front();
+                    msg.cmd = _parseQueue.get_n_pop_front();
+                    uint8_t nbByte = _parseQueue.get_n_pop_front();
 
-                    for(int i = 0; i < nbByte; i++)
+                    for (int i = 0; i < nbByte; i++)
                     {
-                        msg.data.push_back(parseQueue.get_n_pop_front());
+                        msg.data.push_back(_parseQueue.get_n_pop_front());
                     }
 
-                    uint16_t checkResult= (uint16_t)(parseQueue.get_n_pop_front()<<8);
-                    checkResult += parseQueue.get_n_pop_front();
+                    uint16_t checkResult = (uint16_t)(_parseQueue.get_n_pop_front() << 8);
+                    checkResult += _parseQueue.get_n_pop_front();
 
-                    //pop the unused end data
-                    parseQueue.pop_front();
+                    // pop the unused end data
+                    _parseQueue.pop_front();
 
                     uint16_t calc_checksum = checkSum(msg.slave, msg.cmd, nbByte, msg.data);
 
                     // if the checksum is bad, drop the packet
-                    if(checkResult != calc_checksum)
+                    if (checkResult != calc_checksum)
                     {
-                        //publisher.publish(msg);
+                        // publisher.publish(msg);
                     }
                 }
             }
